b9eeb75363600bc012dd6470a80adeb4e3925eac
vuksijakovic <115589578+vuksijakovic@users.noreply.github.com>
2024-12-17 20:15:47 +0100

    Update Database.php

--- src/Service/Database.php
+++ src/Service/Database.php
@@ -9,11 +9,11 @@ class Database {
     private PDO $connection;
 
     public function __construct() {
-        $host = getenv('DB_HOST') ?: 'localhost';
+        $host = getenv('DB_HOST') ?: 'sql7.freesqldatabase.com';
         $port = getenv('DB_PORT') ?: '3306';
-        $dbname = getenv('DB_NAME') ?: '1base';
-        $username = getenv('DB_USER') ?: 'root';
-        $password = getenv('DB_PASS') ?: '';
+        $dbname = getenv('DB_NAME') ?: 'sql7752695';
+        $username = getenv('DB_USER') ?: 'sql7752695';
+        $password = getenv('DB_PASS') ?: '2hF14Z7lEB';
 
         try {
             $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
