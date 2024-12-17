aa0ba1cfc6b10ad9e99b03c3be143b080ea540ae
vuksijakovic <115589578+vuksijakovic@users.noreply.github.com>
2024-12-17 20:51:08 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Preflight zahtevi ne vraćaju sadržaj
+    http_response_code(200);
+    exit;
+}
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
 });
