6923a8cc7b850ab57a3db2961c93638e30a3aec1
vuksijakovic <115589578+vuksijakovic@users.noreply.github.com>
2024-12-19 23:52:07 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: https://scandiweb-build.onrender.com"); // Replace with your frontend URL
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header('Content-Type: application/json; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
